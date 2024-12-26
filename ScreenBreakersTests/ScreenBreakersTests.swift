ca690aa2ab83e5228a044b81e6a8f9bca3fa8103
Alex Saveliev <alexvsaveliev@gmail.com>
2024-12-26 18:15:41 -0500

    Initial Commit

---
+++ ScreenBreakersTests/ScreenBreakersTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenBreakersTests.swift
+//  ScreenBreakersTests
+//
+//  Created by Alex Saveliev on 12/26/24.
+//
+
+import Testing
+@testable import ScreenBreakers
+
+struct ScreenBreakersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
