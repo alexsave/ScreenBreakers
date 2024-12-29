74b18bf8a1bf5d1759c0dfc9109dea454da0fe04
Alex Saveliev <alexvsaveliev@gmail.com>
2024-12-29 11:47:56 -0800

    we can detect app open and close

--- ScreenBreakers/ScreenBreakersApp.swift
+++ ScreenBreakers/ScreenBreakersApp.swift
@@ -9,6 +9,24 @@ import SwiftUI
 
 @main
 struct UsageMonitorApp: App {
+    // detect when the app is launched, and write to core data
+    // detect when the app is closed, and write to core data
+
+    // This might be useful later
+    init() {
+        NotificationCenter.default.addObserver(forName: UIApplication.didFinishLaunchingNotification, object: nil, queue: .main) {
+            _ in 
+            // write to core data
+            print("app launched at \(Date())")
+        }
+
+        NotificationCenter.default.addObserver(forName: UIApplication.willTerminateNotification, object: nil, queue: .main) {
+            _ in 
+            // write to core data
+            print("app closed at \(Date())")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
